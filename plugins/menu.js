f789f61479c325a4c756a9e4d96bdf384575c90d
MADHAVI-MAX <madhavimax704@gmail.com>
2025-05-31 18:36:23 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -57,7 +57,7 @@ cmd(
         }
       }
 
-      let madeMenu = `👋 *Hello  ${pushname}*
+      let madeMenu = `👋 *හායි ලස්සන ළමයෝ  ${pushname}*
 
 
 | *MAIN COMMANDS* |
@@ -84,15 +84,15 @@ ${menu.group}
 ${menu.search}
 
 
-🥶𝐌𝐚𝐝𝐞 𝐛𝐲 𝐒_𝐈_𝐇_𝐈_𝐋_𝐄_𝐋🥶
+🥶𝐌𝐚𝐝𝐞 𝐛𝐲 🔱M_A_D_H_A_V_I🔱🥶
 
-> ROBIN MENU MSG
+> MADHAVI MENU MSG
 `;
       await robin.sendMessage(
         from,
         {
           image: {
-            url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20menu.jpg",
+            url: "https://raw.githubusercontent.com/MADHAVI-MAX/BOT-HELPER/refs/heads/main/DFJ.jpg",
           },
           caption: madeMenu,
         },
