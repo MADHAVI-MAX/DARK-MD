516e96e72314b1b0f0eb7d8416e39b119774d431
MADHAVI-MAX <madhavimax704@gmail.com>
2025-06-01 21:11:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "h2slTT7I#CZael-e6sahfbtYZ2mkOlrB2ZGEMULkEZNzW5KsKKfk",
+  SESSION_ID: process.env.SESSION_ID || "MyMgQbQD#EcLcSXTBafAIXemUmV5Org78RO3mEvgDyiIXNFTQlFk",
   MONGODB: process.env.MONGODB || "mongodb://mongo:HmHxzbrVltwByynNERwjepSnTdtzqMhk@maglev.proxy.rlwy.net:10333",
   OWNER_NUM: process.env.OWNER_NUM || "94772906743",
 };
