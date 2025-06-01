058fd1b51b0af0453f31d581d019a15fe57d9a65
MADHAVI-MAX <madhavimax704@gmail.com>
2025-06-01 17:05:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "BfgpLeUyZiZfQe6Er5HMclpFzIdkBDSozAddvLNvz3JW",
+  SESSION_ID: process.env.SESSION_ID || "h2slTT7I#CZael-e6sahfbtYZ2mkOlrB2ZGEMULkEZNzW5KsKKfk",
   MONGODB: process.env.MONGODB || "mongodb://${{MONGO_INITDB_ROOT_USERNAME}}:${{MONGO_INITDB_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}",
   OWNER_NUM: process.env.OWNER_NUM || "94772906743",
 };
